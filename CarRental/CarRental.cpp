040ed681c13ccd3f5783466141c1016db8217830
Artem <bakhtinartyom1205@gmail.com>
2023-02-18 21:08:51 +0100

    Cmake project initialization

---
+++ CarRental/CarRental.cpp
@@ -0,0 +1,12 @@
+﻿// CarRental.cpp : Defines the entry point for the application.
+//
+
+#include "CarRental.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
